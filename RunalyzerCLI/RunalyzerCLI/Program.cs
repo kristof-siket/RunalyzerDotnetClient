161f494ca38773b658a9043998a8c426b526a787
kristof-siket <kristofsiket@gmail.com>
2018-09-19 16:39:15 +0200

    [dotnet] Initial small menu added to the console interface (reason: choose from 2 services)

--- RunalyzerCLI/RunalyzerCLI/Program.cs
+++ RunalyzerCLI/RunalyzerCLI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RunalyzerCLI.Model;
 
 namespace RunalyzerCLI
 {
@@ -6,7 +7,24 @@ namespace RunalyzerCLI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            sportsdirectorsystemContext context = new sportsdirectorsystemContext();
+
+            string username;
+
+            Console.WriteLine("Email address for Runalyzer:");
+            username = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine($"Welcome to Runalyzer CLI! Feel free to take a service from the list below!");
+            Console.WriteLine();
+
+            Console.WriteLine($"1 - Overall stats for a race");
+            Console.WriteLine($"2 - My stats for a race");
+
+            int service = int.Parse(Console.ReadKey().ToString());       
+
+            Console.ReadKey();
         }
     }
 }
